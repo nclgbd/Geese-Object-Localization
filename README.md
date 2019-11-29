3a965f4c4a449542be5eda7f409d7d6a2159871a
Nicole <nicoleguob@gmail.com>
2019-11-28 21:18:57 -0800

    added further documentation

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Geese-Object-Localization
+# Geese Object Detection and Localization using Mask RCNN.
+
+This repo is used to identify the geese in a video feed with the purpose of being used to scare geese off of my cousins lawn. **That's it, that's the tweet.**
+
+The API used to do this object detection was the Mask-RCNN repository, linked [here](https://github.com/matterport/Mask_RCNN), and for information on how to install follow the link. The model was trained on Google Colab, of which a link will be provided after training as been done. There will be example footage of the detection working uploaded later on as well.
+
+To run this, I suggest making an Anaconda environment with the requirements needed to run Mask RCNN.
--- scripts/preprocessing.py
+++ scripts/preprocessing.py
@@ -1,3 +1,5 @@
+''' This file is for preprocessing all of the different geese images I used for creating the model to train off of.
+'''
 import cv2 as cv
 import numpy as np
 import os
@@ -5,16 +7,20 @@ import os
 PRE_PATH = 'preprocessed_geese/'
 POST_PATH = 'geese/images/'
 def process_images():
+    ''' Processes all of the images, and puts them into the training and testing folders.
+    '''
     num = 1
     for goose in os.listdir(PRE_PATH):
         file = os.path.join(PRE_PATH, goose)
         old_file = cv.imread(file)
         prefix = get_prefix(num) + str(num)
-        cv.imwrite(prefix+'.jpg', old_file)
+        cv.imwrite(prefix +'.jpg', old_file)
         print(prefix)
         num += 1
         
 def get_prefix(num):
+    ''' Used to create the numbering for the files.
+    '''
     length = 5
     prepend = '0' * (length - len(str(num)))
     return prepend
